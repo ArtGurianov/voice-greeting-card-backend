f7ae812a6aadd1623a714790055e94d61eaaa6fa
Art Gurianov <artgurianov@yandex.ru>
2020-11-03 14:26:34 +0300

    get all distributors method. (For pin-map on the main page)

--- src/user/distributor/distributor.service.ts
+++ src/user/distributor/distributor.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@nestjs/common';
 // import { ConfigService } from '@nestjs/config'
-// import { InjectRepository } from '@nestjs/typeorm'
+import { InjectRepository } from '@nestjs/typeorm';
+import { Distributor } from './distributor.entity';
+import { DistributorRepository } from './distributor.repository';
 // import { UserRepository } from '../user.repository'
-// import { DistributorRepository } from './manufacturer.repository'
 
 @Injectable()
 export class DistributorService {
-//   public constructor(
-//     // @InjectRepository(UserRepository)
-//     // private readonly userRepo: UserRepository,
-//     // @InjectRepository(DistributorRepository)
-//     // private readonly distributorRepo: DistributorRepository,
-//     // private readonly configService: ConfigService,
-//   ) {}
+  public constructor(
+    // @InjectRepository(UserRepository)
+    // private readonly userRepo: UserRepository,
+     @InjectRepository(DistributorRepository)
+    private readonly distributorRepo: DistributorRepository,
+    // private readonly configService: ConfigService,
+  ) {
+  }
+  async allDistributors(): Promise<Distributor[]> {
+    return this.distributorRepo.find()
+}
 }
--- src/user/distributor/distrubutor.resolver.ts
+++ src/user/distributor/distrubutor.resolver.ts
@@ -1,10 +1,15 @@
-import { Resolver } from '@nestjs/graphql';
-//import { DistributorService } from './distributor.service';
+import { Query, Resolver } from '@nestjs/graphql';
+import { Public } from '../../utils/public.decorator';
+import { Distributor } from './distributor.entity';
+import { DistributorService } from './distributor.service';
 
 @Resolver()
 export class DistributorResolver {
-  //constructor(private readonly distributorService: DistributorService) {}
+  constructor(private readonly distributorService: DistributorService) {}
 
-  
-
+  @Public()
+  @Query(()=>[Distributor])
+  async allDistributors(): Promise<Distributor[]> {
+    return this.distributorService.allDistributors()
+  }
 }
