0ce91506e8ed2cfe599fd9d94c3b5e5b9ab2906d
luc1ph3r <electrocartridge@gmail.com>
2020-11-18 21:52:22 +0300

    Add migrations

---
+++ migrations/1605724945250-Initial.ts
@@ -0,0 +1,36 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Initial1605724945250 implements MigrationInterface {
+    name = 'Initial1605724945250'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "questions" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" text NOT NULL, "options" text array NOT NULL, "answer" text NOT NULL, "cardId" uuid NOT NULL, CONSTRAINT "PK_08a6d4b0f49ff300bf3a0ca60ac" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "cards" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "isPrinted" boolean NOT NULL DEFAULT false, "isActivatedAudio" boolean NOT NULL DEFAULT false, "isActivatedQuestions" boolean NOT NULL DEFAULT false, "isRevoked" boolean NOT NULL DEFAULT false, "distributorId" uuid, "issuedBy" uuid NOT NULL, CONSTRAINT "PK_5f3269634705fdff4a9935860fc" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" text NOT NULL, "password" text NOT NULL, "tokenVersion" integer NOT NULL DEFAULT '0', "role" text NOT NULL DEFAULT 'CUSTOMER', CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "admins" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "userId" uuid NOT NULL, CONSTRAINT "REL_420cf6d31487d2f341b40d52e3" UNIQUE ("userId"), CONSTRAINT "PK_e3b38270c97a854c48d2e80874e" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "customers" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "userId" uuid NOT NULL, CONSTRAINT "REL_b8512aa9cef03d90ed5744c94d" UNIQUE ("userId"), CONSTRAINT "PK_133ec679a801fab5e070f73d3ea" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "distributors" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "userId" uuid NOT NULL, CONSTRAINT "REL_ab5e8dcf3645029e6a74661ea6" UNIQUE ("userId"), CONSTRAINT "PK_a3741291eb0af96f795b25d90b4" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "manufacturers" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "userId" uuid NOT NULL, CONSTRAINT "REL_0d43aa691a9c863f33a167a217" UNIQUE ("userId"), CONSTRAINT "PK_138520de32c379a48e703441975" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "questions" ADD CONSTRAINT "FK_2dad75daf4b8912b755b32a71c5" FOREIGN KEY ("cardId") REFERENCES "cards"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "admins" ADD CONSTRAINT "FK_420cf6d31487d2f341b40d52e37" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "customers" ADD CONSTRAINT "FK_b8512aa9cef03d90ed5744c94d7" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "distributors" ADD CONSTRAINT "FK_ab5e8dcf3645029e6a74661ea6f" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "manufacturers" ADD CONSTRAINT "FK_0d43aa691a9c863f33a167a2171" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "manufacturers" DROP CONSTRAINT "FK_0d43aa691a9c863f33a167a2171"`);
+        await queryRunner.query(`ALTER TABLE "distributors" DROP CONSTRAINT "FK_ab5e8dcf3645029e6a74661ea6f"`);
+        await queryRunner.query(`ALTER TABLE "customers" DROP CONSTRAINT "FK_b8512aa9cef03d90ed5744c94d7"`);
+        await queryRunner.query(`ALTER TABLE "admins" DROP CONSTRAINT "FK_420cf6d31487d2f341b40d52e37"`);
+        await queryRunner.query(`ALTER TABLE "questions" DROP CONSTRAINT "FK_2dad75daf4b8912b755b32a71c5"`);
+        await queryRunner.query(`DROP TABLE "manufacturers"`);
+        await queryRunner.query(`DROP TABLE "distributors"`);
+        await queryRunner.query(`DROP TABLE "customers"`);
+        await queryRunner.query(`DROP TABLE "admins"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TABLE "cards"`);
+        await queryRunner.query(`DROP TABLE "questions"`);
+    }
+
+}
