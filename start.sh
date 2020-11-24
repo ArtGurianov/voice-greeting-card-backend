e64a5e75951bf3b065c69232dcf8cfe81ad303fe
luc1ph3r <electrocartridge@gmail.com>
2020-11-24 17:03:45 +0300

    Make scripts more compatible

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ apply_migrations() {
     npx ts-node ./node_modules/typeorm/cli.js migration:run -f src/config/typeormConnectionOptions.ts
 }
 
-if [ "$NODE_ENV" == "development" ]
+if [ "$NODE_ENV" = "development" ]
 then
     if apply_migrations; then echo "MIGRATIONS ARE APPLIED TO THE SCHEMA"; else exit 1; fi
     # if generate_migrations; then echo "MIGRATIONS ARE GENERATED"; else exit 1; fi
