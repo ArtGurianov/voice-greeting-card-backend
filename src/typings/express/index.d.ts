5162d1d48bb69df90522548915ddc9303993e935
luc1ph3r <electrocartridge@gmail.com>
2020-12-15 13:34:47 +0300

    Fix custom types compilation

---
+++ src/typings/express/index.d.ts
@@ -0,0 +1,24 @@
+import {JwtPayload} from '../../types/jwtPayload';
+
+declare module 'express' {
+  /**
+   * Used to inflate a new property on a Request object. The type merging is
+   * performed automatically.
+   * Usage:
+   *
+   * ```js
+   * import { Req } from '@nestjs/common';
+   * import { Request } from 'express';
+   *
+   * // ...
+   * @Get()
+   * async someEndpoint(@Req() req: Request) {
+   *   console.log(req.jwtPayload); // is accessible now
+   * }
+   * // ...
+   * ```
+   */
+  interface Request {
+    jwtPayload?: JwtPayload
+  }
+}
